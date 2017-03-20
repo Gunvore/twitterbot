6acb06c36d7ca9e9c32e2a6cb0e066b1aa02eb19
Nicolas de P <deplacenicolas@gmail.com>
2017-03-20 19:28:02 +0000

    V1 fuctnional

--- bot.js
+++ bot.js
@@ -3,46 +3,129 @@ var config = require('./config.js');
 
 var Twitter = new twit(config);  
 
-// RETWEET BOT ==========================
+// STREAM USER BOT ==========================
 
-// find latest tweet according the query 'q' in params
-var retweet = function() {  
-    var params = {
-        q: '#slugwar',  // REQUIRED
-        result_type: 'recent'
+var stream = Twitter.stream('statuses/filter', { follow: config.Users_tracking_list })
+
+stream.on('tweet', function (tweet) {
+
+  Twitter.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
+    if (err) {
+        console.log('---');
+        console.log("Error: " + err.message);
+      } else {
+        console.log('---');
+        console.log('retweet:', tweet.user.screen_name);
+      }
+  })
+
+})
+
+
+
+
+// STREAM HASH BOT ==========================
+var stream2 = Twitter.stream('statuses/filter', { track: config.Hash_tracking_list })
+
+stream2.on('tweet', function (tweet) {
+ console.log(tweet.user.screen_name);
+  var url = findUrls(tweet.text);
+  var m =  url + " " + String(tweet.text).match(/#\w+/g);
+  
+
+  if(url!='' && tweet.user.screen_name != "SlugWar"){
+   Twitter.post('statuses/update', { status: m }, function (err, data, response) {
+          if (err) {
+              console.log('---');
+              console.log("Error: " + err.message);
+            } else {
+              console.log('---');
+              console.log('twitter:',  m);
+            }
+        })
+   
+  }
+   
+})
+
+
+//STREAM follow to reply thanks
+var stream3 = Twitter.stream('user');  
+stream3.on('follow', function (event) {
+  var screenName = event.source.screen_name;
+  var text = "Hi "+screenName+" and thank you for following me. I will retweet everything you say with #slugwarapp related to video games.";
+  Twitter.post("direct_messages/new" , { screen_name:screenName, text:text}, function (err, data, response) {
+    if (err) {
+        console.log('---');
+        console.log("Error: " + err.message);
+      } else {
+        console.log('---');
+        console.log('private message sent:', screenName);
+      
+        Twitter.post('friendships/create', {
+          'screen_name': screenName,
+          'follow': true
+        }, function (err, data, response) {
+          if (err) {
+              console.log('---');
+              console.log("Error: " + err.message);
+            } else {
+              console.log('---');
+              console.log('followed:', screenName);
+            }
+        })
+        
+      }
+  })
+})
+ 
+
+
+
+
+// retweet HASH BOT ==========================
+var stream4 = Twitter.stream('statuses/filter', { track: config.Hash_Retweet_list })
+
+stream4.on('tweet', function (tweet) {
+
+  Twitter.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
+    if (err) {
+        console.log('---');
+        console.log("Error: " + err.message);
+      } else {
+        console.log('---');
+        console.log('retweet:', tweet.user.screen_name);
+      }
+  })
+
+})
+
+
+      
+/**
+ * A utility function to find all URLs - FTP, HTTP(S) and Email - in a text string
+ * and return them in an array.  Note, the URLs returned are exactly as found in the text.
+ * 
+ * @param text
+ *            the text to be searched.
+ * @return an array of URLs.
+ */
+function findUrls( text )
+{
+    var source = (text || '').toString();
+    var urlArray = [];
+    var url;
+    var matchArray;
+
+    // Regular expression to find FTP, HTTP(S) and email URLs.
+    var regexToken = /(((ftp|https?):\/\/)[\-\w@:%_\+.~#?,&\/\/=]+)|((mailto:)?[_.\w-]+@([\w][\w\-]+\.)+[a-zA-Z]{2,3})/g;
+
+    // Iterate through any URLs in the text.
+    while( (matchArray = regexToken.exec( source )) !== null )
+    {
+        var token = matchArray[0];
+        urlArray.push( token );
     }
-    // for more parameters, see: https://dev.twitter.com/rest/reference/get/search/tweets
 
-    Twitter.get('search/tweets', params, function(err, data) {
-      // if there no errors
-        if (!err) {
-          // grab ID of tweet to retweet
-          if(data.statuses[0]){
-            var retweetId = data.statuses[0].id_str;
-            // Tell TWITTER to retweet
-            Twitter.post('statuses/retweet/:id', {
-                id: retweetId
-            }, function(err, response) {
-                if (response) {
-                    console.log('Retweeted!!!');
-                }
-                // if there was an error while tweeting
-                if (err) {
-                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
-                }
-            });
-          }else{
-              console.log('Not twitts');
-          }
-        }
-        // if unable to Search a tweet
-        else {
-          console.log('Something went wrong while SEARCHING...');
-        }
-    });
+    return urlArray;
 }
-
-// grab & retweet as soon as program is running...
-retweet();  
-// retweet in every 50 minutes
-setInterval(retweet, 5000);  
--- config.js
+++ config.js
@@ -9,5 +9,8 @@ module.exports = {
   consumer_key: 'zzwqMMzksAcYi42YF0cUDXr6v',  
   consumer_secret: 'mmfu0bNHhqLAKwDpzvukX3sbdrg6BA7r9u0fhKFLCCyKvEvMie',
   access_token: '3652181597-LGFJLfAseeYptJURQom17oSorpvABepZN4ptMAw',  
-  access_token_secret: 'zADg4RnGK0BT0kfjrs63mlI8bIuGYljFhk432BMnmRPZL'
+  access_token_secret: 'zADg4RnGK0BT0kfjrs63mlI8bIuGYljFhk432BMnmRPZL',
+  Users_tracking_list: ['1946564258'],
+  Hash_tracking_list: ['#indiegames', '#mobileapps', '#indieapps', '#indieandroid', '#indiegame', '#indiedev'],
+  Hash_Retweet_list: ['#slugwarapp']
 }
