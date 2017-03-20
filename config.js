ff17c28c794f785daebe1f203592c5fcd40668ed
Nicolas de P <deplacenicolas@gmail.com>
2017-03-20 12:49:09 +0000

    premier commit

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key : zzwqMMzksAcYi42YF0cUDXr6v
+ * consumer_secret : mmfu0bNHhqLAKwDpzvukX3sbdrg6BA7r9u0fhKFLCCyKvEvMie
+ * access_token : 3652181597-LGFJLfAseeYptJURQom17oSorpvABepZN4ptMAw
+ * access_token_secret : zADg4RnGK0BT0kfjrs63mlI8bIuGYljFhk432BMnmRPZL
+ */
+module.exports = {  
+  consumer_key: 'zzwqMMzksAcYi42YF0cUDXr6v',  
+  consumer_secret: 'mmfu0bNHhqLAKwDpzvukX3sbdrg6BA7r9u0fhKFLCCyKvEvMie',
+  access_token: '3652181597-LGFJLfAseeYptJURQom17oSorpvABepZN4ptMAw',  
+  access_token_secret: 'zADg4RnGK0BT0kfjrs63mlI8bIuGYljFhk432BMnmRPZL'
+}
